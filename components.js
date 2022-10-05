806a408f84106e4bb6310a26182f215157dfa976
Carson <trongld21@gmail.com>
2022-10-05 17:25:30 +0700

    8i hologram

--- components.js
+++ components.js
@@ -1,95 +1,51 @@
-// Create a plane material which will receive the shadows.
-AFRAME.registerComponent('shadow-material', {
-    init: function(){
-        let mesh = this.el.getObject3D('mesh');
-        if (!mesh){return;}
-        mesh.material = new THREE.ShadowMaterial();
-        mesh.material.opacity = 0.2;
-    }
-});
-AFRAME.registerComponent('shadow-opts', {
-    init: function(){
-        // Configure some pretty shadows.
-        let light = this.el.getObject3D('light');
-        if (!light){return;}
-        light.lookAt(0, 0, 0);
-        light.castShadow = true;
-        light.shadow.bias = 0.001;
-        const shadowDistance = 8;
-        light.shadow.camera.top = shadowDistance;
-        light.shadow.camera.bottom = -shadowDistance;
-        light.shadow.camera.left = -shadowDistance;
-        light.shadow.camera.right = shadowDistance;
-
-        light.shadow.camera.near = 0.1;
-        light.shadow.camera.far = 50;
-        light.shadow.radius = 2;
-        light.shadow.mapSize.width = 1024;
-        light.shadow.mapSize.height = 1024;
-    }
-});
-
-AFRAME.registerComponent('holostream-component', {
+AFRAME.registerComponent('eightiControlsComponent', {
     schema: {
-        'url': {default: ''},
-        'scale': {default: 6},
+        'size': {default: 1},  // hologram starting size
     },
     init() {
-        this.showedPrompt = false;
-
-        const holoStreamOptions = {
-            debugEnabled: false,
-            targetCanvasID: '',
-            threeScene: document.querySelector("a-scene").object3D,
-            threeCamera: document.getElementById("camera").object3D, 
-            threeRenderer: this.el.sceneEl.renderer,
-            hideUI: true,
-            overrideRender: true,
-            showPlayButtonOnLoadingScreen: false,
-        }
-        this.holoStream = new HoloStream(holoStreamOptions);
-        this.holoStream.openURL(this.data.url);
-
-        this.data.scale = 2;
-
-        this.placeHologram = (event) => {
-    
-            const mesh = this.holoStream.getThreeMesh();
-
-            mesh.scale.set(this.data.scale, this.data.scale, this.data.scale);
-            mesh.castShadow = true;
-            this.el.object3D.add(mesh);
-
-            const maxS = 1;  // scale to this value
-            const minS = maxS / 100;  // scale from this value
-            this.el.setAttribute('scale', `${minS} ${minS} ${minS}`);
-            this.el.setAttribute('visible', 'true');
-    
-            this.holoStream.handlePlay(true);  // begin playback
-    
-            this.el.setAttribute('animation__scale', {
-                property: 'scale',
-                to: `${maxS} ${maxS} ${maxS}`,
-                easing: 'easeOutExpo',
-                delay: 1000,
-                dur: 750,
-            });
-        }
+        const holo = this.el.components.hologram
+        this.camera = this.el.sceneEl.querySelector('a-camera')
+        this.ground = document.getElementById('ground')
+        this.el.addEventListener('oncanplay', (evt) => {
+            this.prompt.style.display = 'block'
+            const scene = this.el.sceneEl
+            this.el.setAttribute('visible', true)
+            this.placeHologram = (event) => {
+                // place at touchpoint
+                const touchPoint = event.detail.intersection.point
+                this.el.setAttribute('position', touchPoint)
+                const camRot = this.camera.getAttribute('rotation')
+                const thisRot = this.el.getAttribute('rotation')
+                this.el.setAttribute('rotation', `${thisRot.x} ${camRot.y} ${thisRot.z}`)
+                // animate hologram in from a small scale to its end scale
+                const minS = this.data.size / 50
+                const maxS = this.data.size
+                this.el.setAttribute('scale', `${minS} ${minS} ${minS}`)
+                this.el.setAttribute('animation', {
+                    property: 'scale',
+                    to: `${maxS} ${maxS} ${maxS}`,
+                    easing: 'easeOutElastic',
+                    dur: 500,
+                    delay: 100,
+                })
+                // Commands can be sent directly to the hologram component to control playback:
+                // 'play()'
+                // 'pause()'
+                this.el.components.hologram.play()
+                // Only show the progress bar if this is not a live broadcast
+                if (holo.duration !== Infinity) this.progress.style.display = 'block'
+                this.ground.setAttribute('visible', true)
+            }
+            this.ground.addEventListener('mousedown', this.placeHologram, {once: true})
+        }, {once: true})
     },
     tick() {       
-        // Show 'Tap to Place Hologram'
-        if (!this.showedPrompt) {
-            document.getElementById("zappar-placement-ui").addEventListener('click', this.placeHologram, {once: true});
-            this.showedPrompt = true;
+        const holo = this.el.components.hologram
+        if (holo && holo.duration !== Infinity && !holo.paused && this.playProgress != null) {
+        this.playProgress.style.display = 'block'
+        // Playback progress can be tracked using the 'currentTime' and 'duration' getters.
+        this.playProgress.style.width =
+            `${(100 * (holo.currentTime / holo.duration))}%`
         }
-    },
-});
-AFRAME.registerPrimitive('holostream-entity', {
-    defaultComponents: {
-        holostream: {},
-    },
-    mappings: {
-        'src': 'holostream-component.url',
-        'holo-scale': 'holostream-component.scale',
     },
 });
